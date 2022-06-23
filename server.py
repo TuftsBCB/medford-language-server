54c2624ebba2bd6a664bf5ff1b0b0269a250a5aa
liam-strand <Liam.Strand@tufts.edu>
2022-06-23 11:25:47 -0400

    refactor to make obvious what is an LSP method and what is not

--- server.py
+++ server.py
@@ -8,18 +8,16 @@ markup language. Validation is provided by Polina Shpilker's parser. LSP
 bindings are provided by the pygls library.
 
 """
-from timeit import default_timer
 from typing import Union
 
 from pygls.lsp.methods import TEXT_DOCUMENT_DID_CHANGE, TEXT_DOCUMENT_DID_OPEN
 from pygls.lsp.types import (
-    Diagnostic,
     DidChangeTextDocumentParams,
     DidOpenTextDocumentParams,
 )
 from pygls.server import LanguageServer
 
-from . import medford_helpers
+from .medford_helpers import validate_syntax
 
 # The INFO logging level shows most messages passed between the server
 # and the client
@@ -36,7 +34,25 @@ class MEDFORDLanguageServer(LanguageServer):
 medford_server = MEDFORDLanguageServer()
 
 
-def _validate(
+#### #### #### LSP METHODS #### #### ####
+
+
+@medford_server.feature(TEXT_DOCUMENT_DID_CHANGE)
+def did_change(ls: MEDFORDLanguageServer, params: DidChangeTextDocumentParams):
+    """Text document did change notification."""
+    _generate_syntactic_diagnostics(ls, params)
+
+
+@medford_server.feature(TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls: MEDFORDLanguageServer, params: DidOpenTextDocumentParams):
+    """Text document did open notification."""
+    _generate_syntactic_diagnostics(ls, params)
+
+
+#### #### #### HELPERS #### #### ####
+
+
+def _generate_syntactic_diagnostics(
     ls: MEDFORDLanguageServer,
     params: Union[DidOpenTextDocumentParams, DidChangeTextDocumentParams],
 ) -> None:
@@ -49,19 +65,6 @@ def _validate(
     # Get the current document from the text editor
     doc = ls.workspace.get_document(params.text_document.uri)
 
-    #
-    (details, diagnostics) = medford_helpers.validate_syntax(doc)
+    (_, diagnostics) = validate_syntax(doc)
 
     ls.publish_diagnostics(doc.uri, diagnostics)
-
-
-@medford_server.feature(TEXT_DOCUMENT_DID_CHANGE)
-def did_change(ls: MEDFORDLanguageServer, params: DidChangeTextDocumentParams):
-    """Text document did change notification."""
-    _validate(ls, params)
-
-
-@medford_server.feature(TEXT_DOCUMENT_DID_OPEN)
-async def did_open(ls: MEDFORDLanguageServer, params: DidOpenTextDocumentParams):
-    """Text document did open notification."""
-    _validate(ls, params)
